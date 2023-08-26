eb606e21c6ab40a28ad31ab0db7f6f76cc44a23b
amal-work <amalsudharika1@gmail.com>
2023-08-26 18:39:44 +0900

    comment user name

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -10,7 +10,7 @@
                 <div class="comment_user">
                     <div class="pic"><img src="{{url('/images/Brittan_Wheeler.png')}}" class="img-fluid" alt=""></div>
                     <div class="member-info">
-                        <h4>William Anderson</h4>
+                        <h4>{{$comment->user->name}}</h4>
                         <span>CTO</span>
                     </div>
                 </div>
